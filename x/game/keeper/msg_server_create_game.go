06004460102a780c387af5f762bec4c8965b13d0
wwwhickup <jenakarrasco@gmail.com>
2022-10-31 10:16:45 -0700

    message: createGame update

--- x/game/keeper/msg_server_create_game.go
+++ x/game/keeper/msg_server_create_game.go
@@ -3,15 +3,29 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"tictactoe/x/game/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+func initialStatus() []int32 {
+	initialStatus := make([]int32, 9)
+	return initialStatus
+}
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
 	_ = ctx
+	game := types.Game{
+		Creator:   msg.Creator,
+		Inviter:   msg.Inviter,
+		Status:    initialStatus(),
+		Winner:    int32(0),
+		Completed: false,
+	}
+	k.AppendGame(ctx, game)
 
 	return &types.MsgCreateGameResponse{}, nil
 }
